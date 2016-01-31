536e9a5da38ec066fb449adaa6b9a25b7d976407
Pranav Harathi <phrocks254@gmail.com>
2016-01-31 17:08:31 -0600

    Updated Search functionality
    Wrapper now returns Search objects for easier access to information in
    JSON response for search queries.

--- twitter_parser.py
+++ twitter_parser.py
@@ -3,6 +3,17 @@
 date: 01-30-16
 
 Parses Twitter for recent mentions, retweets, etc. to evaluate online presence of presidential candidates.
+
+Example usage of parser:
+
+1. Search query = "Adele"
+from twitter_parser import *
+wrapper = new TwitterWrapper(c_key, c_secret, a_token, a_token_secret)
+search = wrapper.search("Adele")
+# list of tweets
+tweet_list = search.tweets
+
+
 """
 
 import requests
@@ -29,13 +40,54 @@ class TwitterWrapper():
 
 	def search(self, query):
 		"""
-		Returns Requests object specifically for a search query
+		Returns Search object
 		"""
-		return requests.get("https://api.twitter.com/1.1/search/tweets.json?q=" + query, auth=self.auth_obj)
+		return Search(requests.get("https://api.twitter.com/1.1/search/tweets.json?q=" + query, auth=self.auth_obj))
 
+class Search():
 
+	def __init__(self, r_obj):
+		self.r_obj = r_obj
+		self.tweets = []
+		for x in json.loads(self.r_obj.text)["statuses"]:
+			tweet = Tweet()
+			tweet.dict = x
+			tweet.created_at = x["created_at"]
+			tweet.id = x["id"]
+			tweet.text = x["text"]
+			tweet.source = x["source"]
+			tweet.in_reply_to_status_id = x["in_reply_to_status_id"]
+			tweet.in_reply_to_user_id = x["in_reply_to_user_id"]
+			tweet.user = User()
+			tweet.user.dict = x["user"]
+			tweet.user.id = x["user"]["id"]
+			tweet.user.name = x["user"]["name"]
+			tweet.user.screen_name = x["user"]["screen_name"]
+			tweet.user.location = x["user"]["location"]
+			tweet.user.followers_count = x["user"]["followers_count"]
+			self.tweets.append(tweet)
 
+class Tweet():
 
+	def __init__(self):
+		self.created_at = ""
+		self.id = ""
+		self.text = ""
+		self.source = ""
+		self.in_reply_to_status_id = ""
+		self.in_reply_to_user_id = ""
+		self.user = None
+		self.dict = None
 
+	def __repr__(self):
+		return unicode(self.text).encode('utf8')
 
-# usernames: realDonaldTrump, tedcruz, BernieSanders, HillaryClinton
+class User():
+
+	def __init__(self):
+		self.id = ""
+		self.name = ""
+		self.screen_name = ""
+		self.location = ""
+		self.followers_count = 0
+		self.dict = None
